f0a806f22feed7db0a4fe65ce6faafeb2fb030dd
PerchunPak <git@perchun.it>
2023-02-25 23:39:25 +0100

    Switch to SimpleAnalytics instead of Vercel one

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
 import { Provider as RWBProvider } from "react-wrap-balancer";
 import cx from "classnames";
 import localFont from "@next/font/local";
 import { Inter } from "@next/font/google";
+import Script from "next/script";
 
 const sfPro = localFont({
   src: "../styles/SF-Pro-Display-Medium.otf",
@@ -27,7 +27,15 @@ export default function MyApp({
           <Component {...pageProps} />
         </div>
       </RWBProvider>
-      <Analytics />
+      <Script src="https://simpleanal.perchun.it/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://simpleanal.perchun.it/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
     </>
   );
 }
