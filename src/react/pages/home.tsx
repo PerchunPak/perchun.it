3935b429ad5d14b38a6da54b589720c08e945fb2
Perchun Pak <git@perchun.it>
2023-04-04 09:40:16 +0000

    04.04.2023

---
+++ src/react/pages/home.tsx
@@ -0,0 +1,51 @@
+import Balancer from "react-wrap-balancer";
+import { motion } from "framer-motion";
+import { FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
+import config from "@/lib/config";
+import useWindowSize from "@/lib/hooks/use-window-size";
+
+export default function Home() {
+  const { isDesktop } = useWindowSize();
+
+  return (
+    <>
+      <motion.div
+        className="stop-wrapping max-w-xl px-5 xl:px-0"
+        initial="hidden"
+        whileInView="show"
+        animate="show"
+        viewport={{ once: true }}
+        variants={{
+          hidden: {},
+          show: {
+            transition: {
+              staggerChildren: 0.15,
+            },
+          },
+        }}
+      >
+        <motion.h1
+          className="bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[0.03em] text-transparent drop-shadow-sm md:text-7xl md:leading-[5rem]"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <Balancer>{config.title}</Balancer>
+        </motion.h1>
+        <motion.p
+          className={`mt-6 text-center text-gray-500 md:text-xl ${
+            isDesktop ? "stop-wrapping" : ""
+          }`}
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          {isDesktop ? config.aboutMe : <Balancer>{config.aboutMe}</Balancer>}
+        </motion.p>
+        <motion.div
+          className="mx-auto mt-6 flex items-center justify-center space-x-3"
+          variants={FADE_DOWN_ANIMATION_VARIANTS}
+        >
+          <slot name="my-socials" />
+        </motion.div>
+      </motion.div>
+      {/* <slot name="my-projects" /> */}
+    </>
+  );
+}
