ed2a5db3674c768db26f7a056e793ced049f536a
PerchunPak <git@perchun.it>
2024-01-02 07:39:35 +0100

    Add simpleanalytics.com

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,19 @@
 	import 'iconify-icon';
 </script>
 
+<svelte:head>
+	<!-- Simple Analytics - 100% privacy-first analytics -->
+	<!-- Simple Analytics - 100% privacy-first analytics -->
+	<script async defer src="https://simpleanal.perchun.it/latest.js"></script>
+	<noscript>
+		<img
+			src="https://simpleanal.perchun.it/noscript.gif"
+			alt=""
+			referrerpolicy="no-referrer-when-downgrade"
+		/>
+	</noscript>
+</svelte:head>
+
 <main class="m-0 w-full min-h-screen">
 	<slot />
 </main>
