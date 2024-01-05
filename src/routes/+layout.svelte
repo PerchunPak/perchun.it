9caff53f022861099e454bd9ef875bab7adf0c45
PerchunPak <git@perchun.it>
2024-01-05 18:39:17 +0100

    Add some meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,10 @@
 </script>
 
 <svelte:head>
+	<title>Perchun's portfolio</title>
+	<meta name="keywords" content="Perchun, portfolio, programmer, backend, software">
+	<meta name="description" content="16 years old Junior+ Backend/Software developer!">
+
 	<script async defer src="https://simpleanal.perchun.it/latest.js"></script>
 	<noscript>
 		<img
