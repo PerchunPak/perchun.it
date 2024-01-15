76ac0effa6f50ce12edaf3c5ae55519957dec1ab
PerchunPak <git@perchun.it>
2024-01-15 01:54:17 +0100

    Use skeleton.dev as a UI library
    I like it more, and I don't have to write shitty JS anymore!

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,44 @@
-<script>
-	import '../app.css';
-	import 'iconify-icon';
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<svelte:head>
-	<script async defer src="https://simpleanal.perchun.it/latest.js"></script>
-	<noscript>
-		<img
-			src="https://simpleanal.perchun.it/noscript.gif"
-			alt=""
-			referrerpolicy="no-referrer-when-downgrade"
-		/>
-	</noscript>
-</svelte:head>
-
-<main class="m-0 w-full min-h-screen">
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Skeleton</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/EXqV7W8MtY"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Twitter
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
 	<slot />
-</main>
+</AppShell>
