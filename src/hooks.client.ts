81b89f8e4b0041d62a0af9a9661f62657700cec1
PerchunPak <git@perchun.it>
2024-01-20 01:10:36 +0100

    Do not enable Sentry during development

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -2,6 +2,7 @@ import { handleErrorWithSentry, Replay } from "@sentry/sveltekit";
 import * as Sentry from '@sentry/sveltekit';
 
 Sentry.init({
+  enabled: import.meta.env.PROD,
   dsn: 'https://5db59bfd06d1657b961bc70482a936a5@o4504254006689792.ingest.sentry.io/4506596167385088',
   tracesSampleRate: 1.0,
 
