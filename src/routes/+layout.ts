dd2c2bc6bf72bf5865030379e4703bb9c375ce0d
PerchunPak <git@perchun.it>
2024-02-08 19:00:35 +0100

    Run `pnpm format`

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,20 +1,20 @@
-import { currentProjectIndex } from "$lib/stores";
-import { projectsMetadata } from "$lib/projects-metadata";
+import { currentProjectIndex } from '$lib/stores';
+import { projectsMetadata } from '$lib/projects-metadata';
 
 function extractProjectFromUrl(url: string | null): string | null {
-  if (url === null) return null;
-  
-  const parts = url.split("/")
-  if (parts.length === 2) {
+	if (url === null) return null;
+
+	const parts = url.split('/');
+	if (parts.length === 2) {
 		return parts[1];
 	}
 
-  return null;
+	return null;
 }
 
 export function load({ route: { id: routeId } }): void {
-  const projectSlug = extractProjectFromUrl(routeId)
-  if (projectSlug === null) return;
+	const projectSlug = extractProjectFromUrl(routeId);
+	if (projectSlug === null) return;
 
 	currentProjectIndex.set(
 		projectsMetadata.findIndex((project) => {
